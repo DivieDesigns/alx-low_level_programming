a23f8aa82012c5e3fe68c048fa963f5a81d9e73c
Aluebho Jesuite <106759682+DivieDesigns@users.noreply.github.com>
2022-07-21 18:15:21 +0100

    ss

---
+++ tiral.c
@@ -0,0 +1,16 @@
+int print(int nb)
+{
+    if (nb < 0) 
+    {
+        return (0);
+    }
+    printf("%d", nb + print(nb - 1));
+    nb --;
+    return (nb);
+}
+
+int main(void)
+{
+    print(4);
+    return (0);
+}
